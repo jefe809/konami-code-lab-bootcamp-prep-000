72ba0e91e6710a6fed59636b2de378d686ee3974
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-28 21:02:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,17 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(e) {
-  // Write your JavaScript code inside the init() function
-  let index = 0;
-  const key = parseInt(e.detail || e.location);
-  if (key === code[index]){
-    index++;
-    if (index === code.length){
-      alert("Congrats!");
-      index = 0;
-    }
-  }else {
-    index = 0;
-  }
-
+function init() {
+  
 }
