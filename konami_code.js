403073957fb06f549d009ea4e31df330178cd94c
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-28 21:09:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,5 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   document.body.addEventListener('keydown', function(e){
     const key = parseInt(e.detail || e.which)
+    if(key === code[index]){
+      index++;
+    }
   });
 }
