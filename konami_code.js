394a5d5b7b62a462d7290760f474b499cf563877
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-28 21:13:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
     const key = parseInt(e.detail || e.which)
     if(key === code[index]){
       index++;
-      if(index === code.length){
+      if(index === code.length -1){
         alert("Hurray");
         index = 0;
       }
